e4daf90cb8498de8eca052966cbcdae073e21e6d
Jing Guo <guojingling@hotmail.com>
2019-02-16 23:03:36 +1100

    Transpile and bundling

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var path = require('path');
-var opn = require('opn');
+import express from 'express';
+import path from 'path';
+import opn from 'opn';
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
+
 
 const port = 3000;
 const app = express();
+const compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler,{
+  noInfor: true,
+  publicPath: config.output.publicPath
+}));
+
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
@@ -13,6 +23,6 @@ app.listen(port, function(err) {
   if (err){
     console.log(err);
   }else{
-    opn('http://localhost:'+port);
+    opn('http://localhost:'+port, {app:['Chrome', '--incognito']});
   }
 });
