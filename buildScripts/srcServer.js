2bb4f7b91a259e154e68ca26e21d6e4a9e88afb9
Jing Guo <guojingling@hotmail.com>
2019-03-10 16:29:39 +1100

    complete HTTP and Mock session

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -22,6 +22,14 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function(req, res){
+  res.json([
+    {"id":1, "frist Name":"Bob", "LastName":"Smith", "email":"bob@gmail.com"},
+    {"id":2, "frist Name":"Tammy", "LastName":"Norton", "email":"tnorton@yahoo.com"},
+    {"id":3, "frist Name":"Tina", "LastName":"Lee", "email":"lee.tina@hotmail.com"}
+  ]);
+});
+
 app.listen(port, function(err) {
   if (err){
     console.log(err);
